5353ddbace58b88dfe4bd136f9197faa9fde2ac1
T1To48 <tofek.98@gmail.com>
2023-01-28 15:27:58 +0200

    full functioning , some styling left

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let profiles = [
    {},
   {
+   //I've wrote the id key-value for futuristic dynamic purposes & to distinguish between players but it wasn't called at all in my code. 
     id: 1,
     currentScore: 0,
     finalScore: 0,
@@ -31,10 +32,11 @@ let holdBtn=document.querySelector('#hold-btn');
 let newgameBtn=document.querySelector('#new-game-btn');
 //variables-Shortcuts & utilities vaiables
 let turn=1;
-let cScore1=profiles[0].currentScore;
-let fScore1=profiles[0].finalScore;
-let cScore2=profiles[1].currentScore;
-let fScore2=profiles[1].finalScore;
+let cScore1=profiles[1].currentScore;
+let fScore1=profiles[1].finalScore;
+let cScore2=profiles[2].currentScore;
+let fScore2=profiles[2].finalScore;
+let targetScoreVal;
 //dynamic variables
 // let cScorevar=profiles[turn].currentScore;
 // let fScorevar=profiles[turn].finalScore;
@@ -42,9 +44,17 @@ let fScore2=profiles[1].finalScore;
 // let finalScoreVar=document.querySelector(`#plyr-${turn}-final-score`);
 ////Visual-Actions & functions 
 
+
+
 startBtn.addEventListener("click",function(){
-   landingPage.style.display="none";
+   if(targetScore.value.length<1 || targetScore.value<1){
+      alert('please enter a target score')
+   }
+   else {
+      landingPage.style.display="none";
    gamePage.style.display="grid";
+   targetScoreVal=document.querySelector('#target-score').value;
+}
 })
 
 
@@ -52,27 +62,29 @@ holdBtn.addEventListener("click",function(){
    //add current score to final score
       profiles[turn].finalScore+=profiles[turn].currentScore;
    //print out final score
-   document.querySelector(`#plyr-${turn}-final-score`).textContent=profiles[turn].finalScore
+   document.querySelector(`#plyr-${turn}-final-score`).textContent=profiles[turn].finalScore;
    //return the current score to 0
    profiles[turn].currentScore=0;
    //print the current score again
    document.querySelector(`#plyr-${turn}-current-score`).textContent=profiles[turn].currentScore;
    //check who wins or switch turns
-   if(profiles[turn].finalScore===100){
-      //play (turn) won the game alert
+   if(profiles[turn].finalScore==targetScoreVal){
+      //player (turn) won the game alert
+      alert(`Congratulations Player ${turn}, you have won the game`);
       //if you want to play again press newgame
-      //all page display:none
-      //new game button display:initial
+      alert('if you want to play again press the New game button');
    }
-   if(profiles[turn].finalScore>100 && turn===1){
+   if(profiles[turn].finalScore>targetScoreVal && turn===1){
       //player (turn=2) won the game alert
+      alert(`Congratulations Player 2, you have won the game`);
       //if you want to play again press newgame
-      //all page display:none
-      //new game button display:initial
+      alert('if you want to play again press the New game button');
    }
-   if(profiles[turn].finalScore>100 && turn===2){
+   if(profiles[turn].finalScore>targetScoreVal && turn===2){
       //player (turn=1) won the game alert
+      alert(`Congratulations Player 1, you have won the game`);
       //if you want to play again press newgame
+      alert('if you want to play again press the New game button');
       //all page display:none
       //new game button display:initial
    }
@@ -84,7 +96,9 @@ holdBtn.addEventListener("click",function(){
 
 
 rollDiceBtn.addEventListener("click",function(){
-   //generate 2 random numbs
+   //generate 2 random numbs (we can not take the random numbers variables outside of
+  // the function into the global scope , because they will be generated just once when the page is initialised,
+  // thats why they should be regenrated every time the button "roll the dice" is pressed)
    let rndmNumb1=Math.floor(Math.random()*6)+1;
    let rndmNumb2=Math.floor(Math.random()*6)+1;
    //if numbs=6
@@ -100,5 +114,20 @@ rollDiceBtn.addEventListener("click",function(){
    //print  dice-pics that match the result
    dice1.setAttribute('src',`./assets/dice-${rndmNumb1}.png`);
    dice2.setAttribute('src',`./assets/dice-${rndmNumb2}.png`);
+})
+
+newgameBtn.addEventListener("click",function(){
+   landingPage.style.display="flex";
+   gameRules.style.display="none";
+   targetScore.value=0;
+   profiles[1].currentScore=0;
+   profiles[2].currentScore=0;
+   profiles[1].finalScore=0;
+   profiles[2].finalScore=0;
+   currentScoreP1.textContent=0;
+   currentScoreP2.textContent=0;
+   finalScoreP1.textContent=0;
+   finalScoreP2.textContent=0;
+   turn=1;
 })
 
--- test.js
+++ test.js
@@ -1,2 +1,121 @@
-let rndmNumb2=Math.floor(Math.random()*6)+1;
-console.log(rndmNumb2)
+let profiles = [
+   {},
+  {
+   //I've wrote the id key-value for futuristic dynamic purposes & to distinguish between players but it wasn't called at all in my code. 
+    id: 1,
+    currentScore: 0,
+    finalScore: 0,
+  },
+  {
+    id: 2,
+    currentScore: 0,
+    finalScore: 0,
+  },
+];
+
+
+//QUERY-SELECTORS
+let landingPage=document.querySelector('.landing-page-container');
+let gameRules=document.querySelector('.game-rules');
+let targetScore=document.querySelector('#target-score');
+let startBtn=document.querySelector('#btn');
+let dice1=document.querySelector('#dice-1-img');
+let dice2=document.querySelector('#dice-2-img');
+let rollDiceBtn=document.querySelector('#roll-dice-btn');
+let currentScoreP1=document.querySelector('#plyr-1-current-score');
+let currentScoreP2=document.querySelector('#plyr-2-current-score');
+let finalScoreP1=document.querySelector('#plyr-1-final-score');
+let finalScoreP2=document.querySelector('#plyr-2-final-score');
+let hideGamePage=document.querySelector('#hide-game-page');
+let gamePage=document.querySelector('.game-page-container');
+let holdBtn=document.querySelector('#hold-btn');
+let newgameBtn=document.querySelector('#new-game-btn');
+//variables-Shortcuts & utilities vaiables
+let turn=1;
+let cScore1=profiles[1].currentScore;
+let fScore1=profiles[1].finalScore;
+let cScore2=profiles[2].currentScore;
+let fScore2=profiles[2].finalScore;
+//dynamic variables
+// let cScorevar=profiles[turn].currentScore;
+// let fScorevar=profiles[turn].finalScore;
+// let currentScoreVar=document.querySelector(`#plyr-${turn}-current-score`);
+// let finalScoreVar=document.querySelector(`#plyr-${turn}-final-score`);
+////Visual-Actions & functions 
+
+startBtn.addEventListener("click",function(){
+   if(targetScore.value.length<1 || targetScore.value<1){
+      alert('please enter a target score')
+   }
+   else {landingPage.style.display="none";
+   gamePage.style.display="grid";}
+})
+
+
+holdBtn.addEventListener("click",function(){
+   //add current score to final score
+      profiles[turn].finalScore+=profiles[turn].currentScore;
+   //print out final score
+   document.querySelector(`#plyr-${turn}-final-score`).textContent=profiles[turn].finalScore
+   //return the current score to 0
+   profiles[turn].currentScore=0;
+   //print the current score again
+   document.querySelector(`#plyr-${turn}-current-score`).textContent=profiles[turn].currentScore;
+   //check who wins or switch turns
+   if(profiles[turn].finalScore===targetScore.value){
+      //player (turn) won the game alert
+      alert(`Congratulations Player ${turn}, you have won the game`);
+      //if you want to play again press newgame
+      alert('if you want to play again press the New game button');
+   }
+   if(profiles[turn].finalScore>targetScore.value && turn===1){
+      //player (turn=2) won the game alert
+      alert(`Congratulations Player 2, you have won the game`);
+      //if you want to play again press newgame
+      alert('if you want to play again press the New game button');
+   }
+   if(profiles[turn].finalScore>targetScore.value && turn===2){
+      //player (turn=1) won the game alert
+      alert(`Congratulations Player 1, you have won the game`);
+      //if you want to play again press newgame
+      //all page display:none
+      //new game button display:initial
+   }
+  ///switch turns between players
+   else{
+   turn===1?turn=2:turn=1;
+}
+})
+
+
+rollDiceBtn.addEventListener("click",function(){
+   //generate 2 random numbs (we can not take the random numbers variables outside of
+  // the function into the global scope , because they will be generated just once when the page is initialised,
+  // thats why they should be regenrated every time the button "roll the dice" is pressed)
+   let rndmNumb1=Math.floor(Math.random()*6)+1;
+   let rndmNumb2=Math.floor(Math.random()*6)+1;
+   //if numbs=6
+   if(rndmNumb1===6 && rndmNumb2===6){
+      profiles[turn].currentScore=0;
+   }
+   else{
+   profiles[turn].currentScore+=rndmNumb1;
+   profiles[turn].currentScore+=rndmNumb2;
+   }
+   //print out results of current score
+   document.querySelector(`#plyr-${turn}-current-score`).textContent=profiles[turn].currentScore;
+   //print  dice-pics that match the result
+   dice1.setAttribute('src',`./assets/dice-${rndmNumb1}.png`);
+   dice2.setAttribute('src',`./assets/dice-${rndmNumb2}.png`);
+})
+
+newgameBtn.addEventListener("click",function(){
+   landingPage.style.display="flex";
+   gameRules.style.display="none";
+   targetScore.value=0;
+   currentScoreP1.textContent='0';
+   currentScoreP2.textContent='0';
+   finalScoreP1.textContent='0';
+   finalScoreP2.textContent='0';
+   turn=1;
+})
